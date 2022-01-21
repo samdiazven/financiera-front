a3d5bc9ca1193b3c8c7333c382603bf82a687bdc
samuel <samueldiazkoc@gmail.com>
2022-01-21 11:09:40 -0500

    change to production

--- src/apis/constants.js
+++ src/apis/constants.js
@@ -1,4 +1,4 @@
 // PROD
 // export const url = 'http://localhost:3000/api/v1/';
 // DEV
-export const url = "https://saludfinancieradevelop.azurewebsites.net/api";
+export const url = "https://saludfinancieraproduction.azurewebsites.net/api";
