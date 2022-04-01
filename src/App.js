95bf99238459132c9fb767c158b183f9c4d3bfc5
samuel diaz <samuel@gmail.com>
2022-03-31 21:08:36 -0500

    remove 404

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 import Landing from "layouts/Landing";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -12,20 +11,19 @@ import Redirection from "layouts/Redirection";
 import Investments from "layouts/Investments";
 
 export default function App() {
-  return <h1 style={{ textAlign: "center" }}>404</h1>;
-  // return (
-  //   <Provider store={store}>
-  //     <ChakraProvider theme={theme}>
-  //       <Router>
-  //         <Switch>
-  //           <Route path="/admin" component={AdminLayout} />
-  //           <Route path="/redirect" component={Redirection} />
-  //           <Route path={`/auth`} component={SignIn} />
-  //           <Route path="/investment" component={Investments} />
-  //           <Route exact path={`/`} component={Landing} />
-  //         </Switch>
-  //       </Router>
-  //     </ChakraProvider>
-  //   </Provider>
-  // );
+  return (
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/admin" component={AdminLayout} />
+            <Route path="/redirect" component={Redirection} />
+            <Route path={`/auth`} component={SignIn} />
+            <Route path="/investment" component={Investments} />
+            <Route exact path={`/`} component={Landing} />
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </Provider>
+  );
 }
